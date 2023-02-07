3a6614a36f2984254284f090df4664dc6d123fec
oUnknown0 <33472641+oUnknown0@users.noreply.github.com>
2023-02-07 15:12:16 +0800

    pdf

--- YouthActionDotNet/ClientApp/src/Pages/Project/Project.js
+++ YouthActionDotNet/ClientApp/src/Pages/Project/Project.js
@@ -1,6 +1,9 @@
 
 import React from "react"
+import JsPDF from 'jspdf';
 import { Loading } from "../../Components/appCommon"
+import { IconButtonWithText, MultiStepBox, StdButton, IconButton } from "../../Components/common"
+import { StdInput } from "../../Components/input"
 import DatapageLayout from "../PageLayout"
 
 export default class Project extends React.Component {
@@ -132,9 +135,133 @@ export default class Project extends React.Component {
                 error={this.state.error}
                 permissions={this.props.permissions}
                 requestError={this.requestError}
+                // extraComponents = {
+                //     [
+                //         {
+                //             label: "Generate PDF", 
+                //             key: "generatePDF", 
+                //             requiredPerms: ["Create","Update","Delete","Read"],
+                //             component: <GeneratePDF 
+                //             settings={this.props.settings} 
+                //             requestRefresh={this.props.requestRefresh} 
+                //             fieldSettings = {this.props.fieldSettings} 
+                //             data={this.props.data} 
+                //             requestError={this.props.requestError}
+                //             api = {this.settings.api}>
+                //             </GeneratePDF>
+                //         }
+                //     ]
+                // }
                 >
+                {/* {this.state.content.data.map((item, index) => {
+                    return (
+                        <div className="staff-extended">
+                            <PermissionsMap handleUpdate={this.handleUpdate} item={item}></PermissionsMap>
+                        </div>
+                    )
+                })}     */}
             </DatapageLayout>
             )
         }
     }
 }
+
+class GeneratePDF extends React.Component{
+    state={
+        columns: [],
+        pdfReady: false,
+        loading:true,
+    }
+    
+    componentDidMount(){
+        let columns = [];
+        console.log("Hello World");
+        console.log(this.props.fieldSettings);
+        for(var i = 0; i < Object.keys(this.props.fieldSettings).length; i++){
+            columns.push(
+                {
+                    label: Object.keys(this.props.fieldSettings)[i],
+                    key: Object.keys(this.props.fieldSettings)[i],
+                }
+            );
+        }
+        this.setState({
+            columns: columns
+        });
+    }
+
+    reOrderColumns = (index, direction) => {
+        var tempColumns = this.state.columns;
+        if(direction === "up"){
+            if(index > 0){
+                var temp = tempColumns[index];
+                tempColumns[index] = tempColumns[index - 1];
+                tempColumns[index - 1] = temp;
+            }
+        } else {
+            if(index < tempColumns.length - 1){
+                var temp = tempColumns[index];
+                tempColumns[index] = tempColumns[index + 1];
+                tempColumns[index + 1] = temp;
+            }
+        }
+        this.setState({
+            columns: tempColumns
+        });
+    }
+
+    generatePDF = () =>{
+        this.setState({
+            pdfReady : false
+        })
+
+        // Fake loading time to show false sense of progress
+        setTimeout(() => {
+            this.setState({
+                pdfReady : true
+            })}, 1000);
+    }
+    exportPDF = () => {
+
+        const report = new JsPDF('portrait','pt','a4');
+        report.html(document.body).then(() => {
+            report.save(this.props.settings.title + ".pdf");
+        });
+    }
+
+    render(){
+        return (
+            <div className="container-fluid generate-spreadsheet">
+                <div className="column-order">
+                    {this.state.columns.map((column, index) => {
+                        return <div className="column">
+                            <div className="column-order-buttons">
+                                <IconButton className={"invert"} icon={<i className="bi bi-arrow-up"></i>} onClick={() => this.reOrderColumns(index, "up")}></IconButton>
+                                <IconButton className={"invert"} icon={<i className="bi bi-arrow-down"></i>} onClick={() => this.reOrderColumns(index, "down")}></IconButton>
+                            </div>
+                            <div className="column-name">{column.label}</div>
+                        </div>
+                    })}     
+                </div>
+                <div className="generate-actions">
+                    <StdButton onClick={() => this.generatePDF()}>
+                        Generate PDF
+                    </StdButton>
+
+                    {this.state.pdfReady ?
+                    
+                    // <CSVLink data={this.props.data} className={"forget-password"} headers={this.state.columns} filename={this.props.settings.title + ".csv"}>Download</CSVLink>
+                    <button onClick={this.exportPDF} type="button">Export PDF</button>
+                    :
+                    <div className="spinner-border" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </div>
+                    }    
+                </div>
+            </div>
+        )
+    }
+
+    
+}
+
